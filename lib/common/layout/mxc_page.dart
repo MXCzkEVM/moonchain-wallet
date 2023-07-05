2ffdc2b58eaca49782eefeea82df549789b8610a
reasje <rezaaslejeddian@gmail.com>
2023-07-05 17:52:46 +0330

    fix: Formatted changed files

--- lib/common/layout/mxc_page.dart
+++ lib/common/layout/mxc_page.dart
@@ -16,32 +16,32 @@ import 'edit_mode_status_bar.dart';
 const contentPadding = EdgeInsets.symmetric(horizontal: 16);
 
 abstract class MxcPage extends HookConsumerWidget {
-  const MxcPage.internal({
-    Key? key,
-    this.scaffoldKey,
-    required this.children,
-    this.footer,
-    this.appBar,
-    this.bottomNavigationBar,
-    this.childrenPadding,
-    this.useContentPadding = true,
-    this.drawer,
-    this.layout = LayoutType.scrollable,
-    this.onRefresh,
-    this.presenter,
-    this.crossAxisAlignment = CrossAxisAlignment.center,
-    this.scrollController,
-    this.fixedFooter = false,
-    this.floatingActionButton,
-    this.backgroundColor,
-    this.useFooterPadding = true,
-    this.resizeToAvoidBottomInset = true,
-    this.useSplashBackground = false,
-    this.isEditMode = false,
-    this.onAdd,
-    this.onDone,
-    this.useAppBar = false
-  })  : assert(scrollController == null || layout != LayoutType.column),
+  const MxcPage.internal(
+      {Key? key,
+      this.scaffoldKey,
+      required this.children,
+      this.footer,
+      this.appBar,
+      this.bottomNavigationBar,
+      this.childrenPadding,
+      this.useContentPadding = true,
+      this.drawer,
+      this.layout = LayoutType.scrollable,
+      this.onRefresh,
+      this.presenter,
+      this.crossAxisAlignment = CrossAxisAlignment.center,
+      this.scrollController,
+      this.fixedFooter = false,
+      this.floatingActionButton,
+      this.backgroundColor,
+      this.useFooterPadding = true,
+      this.resizeToAvoidBottomInset = true,
+      this.useSplashBackground = false,
+      this.isEditMode = false,
+      this.onAdd,
+      this.onDone,
+      this.useAppBar = false})
+      : assert(scrollController == null || layout != LayoutType.column),
         super(key: key);
 
   const factory MxcPage({
@@ -232,7 +232,7 @@ abstract class MxcPage extends HookConsumerWidget {
     // );
     final presenter = ref.read(homeContainer.actions);
     final state = ref.watch(homeContainer.state);
-    
+
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: getSystemStyle(context, ref, backgroundColor),
       child: Scaffold(
@@ -245,108 +245,118 @@ abstract class MxcPage extends HookConsumerWidget {
         bottomNavigationBar: buildBottomNavigation(context, ref),
         appBar: state.isEditMode
             ? null
-            : useAppBar ? AppBar(
-                elevation: 0.0,
-                leading: MxcCircleButton.icon(
-                  key: const Key("burgerMenuButton"),
-                  icon: Icons.menu_rounded,
-                  shadowRadius: 0,
-                  onTap: () {},
-                  iconSize: 30,
-                  color: ColorsTheme.of(context).primaryText,
-                  iconFillColor: Colors.transparent,
-                ),
-                shadowColor: Colors.transparent,
-                centerTitle: true,
-                actions: [
-                  Padding(
-                    padding: const EdgeInsetsDirectional.only(end: 16),
-                    child: MxcCircleButton.icon(
-                      key: const Key("appsButton"),
-                      icon: MXCIcons.apps_1,
-                      shadowRadius: 30,
-                      onTap: () {
-                        Navigator.of(context).push(
-                          route(
-                            const AppsTab(),
-                          ),
-                        );
-                      },
+            : useAppBar
+                ? AppBar(
+                    elevation: 0.0,
+                    leading: MxcCircleButton.icon(
+                      key: const Key("burgerMenuButton"),
+                      icon: Icons.menu_rounded,
+                      shadowRadius: 0,
+                      onTap: () {},
                       iconSize: 30,
                       color: ColorsTheme.of(context).primaryText,
-                      iconFillColor:
-                          ColorsTheme.of(context).secondaryBackground,
+                      iconFillColor: Colors.transparent,
                     ),
-                  ),
-                ],
-                title: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
+                    shadowColor: Colors.transparent,
+                    centerTitle: true,
+                    actions: [
+                      Padding(
+                        padding: const EdgeInsetsDirectional.only(end: 16),
+                        child: MxcCircleButton.icon(
+                          key: const Key("appsButton"),
+                          icon: MXCIcons.apps_1,
+                          shadowRadius: 30,
+                          onTap: () {
+                            Navigator.of(context).push(
+                              route(
+                                const AppsTab(),
+                              ),
+                            );
+                          },
+                          iconSize: 30,
+                          color: ColorsTheme.of(context).primaryText,
+                          iconFillColor:
+                              ColorsTheme.of(context).secondaryBackground,
+                        ),
+                      ),
+                    ],
+                    title: Row(
+                      mainAxisSize: MainAxisSize.min,
                       children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 10,),
-                          decoration: BoxDecoration(
-                            color: ColorsTheme.of(context).white.withOpacity(0.16),
-                            borderRadius: const BorderRadius.all(Radius.circular(30)),
-                            
-                          ),
-                          child: Row(
-                            children: [
-                              MXCDropDown<String>(
-                                itemList: const ["MXC zkEVM", "Testnet"],
-                                onChanged: (String? newValue) {},
-                                selectedItem: "MXC zkEVM",
-                                icon: const Padding(
-                                  padding: EdgeInsetsDirectional.only(start: 10),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 10,
+                              ),
+                              decoration: BoxDecoration(
+                                color: ColorsTheme.of(context)
+                                    .white
+                                    .withOpacity(0.16),
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(30)),
+                              ),
+                              child: Row(
+                                children: [
+                                  MXCDropDown<String>(
+                                    itemList: const ["MXC zkEVM", "Testnet"],
+                                    onChanged: (String? newValue) {},
+                                    selectedItem: "MXC zkEVM",
+                                    icon: const Padding(
+                                      padding:
+                                          EdgeInsetsDirectional.only(start: 10),
+                                    ),
+                                  ),
+                                  Container(
+                                    height: 8,
+                                    width: 8,
+                                    decoration: BoxDecoration(
+                                        color: ColorsTheme.of(context)
+                                            .systemStatusActive,
+                                        shape: BoxShape.circle),
+                                  ),
+                                  const SizedBox(width: 6),
+                                  Text(FlutterI18n.translate(context, 'online'),
+                                      style: FontTheme.of(context)
+                                          .h7()
+                                          .copyWith(
+                                              fontWeight: FontWeight.w500)),
+                                ],
+                              ),
+                            ),
+                            MXCDropDown<String>(
+                              itemList: [
+                                state.walletAddress != null
+                                    ? Formatter.formatWalletAddress(
+                                        state.walletAddress!.hex)
+                                    : "",
+                              ],
+                              onChanged: (String? newValue) {},
+                              selectedItem: state.walletAddress != null
+                                  ? Formatter.formatWalletAddress(
+                                      state.walletAddress!.hex)
+                                  : "",
+                              textStyle: FontTheme.of(context).h7().copyWith(
+                                  fontSize: 16, fontWeight: FontWeight.w400),
+                              icon: Padding(
+                                padding:
+                                    const EdgeInsetsDirectional.only(start: 0),
+                                child: Icon(
+                                  Icons.arrow_drop_down_rounded,
+                                  size: 32,
+                                  color: ColorsTheme.of(context).purpleMain,
                                 ),
                               ),
-                              Container(
-                                height: 8,
-                                width: 8,
-                                decoration: BoxDecoration(
-                                    color: ColorsTheme.of(context)
-                                        .systemStatusActive,
-                                    shape: BoxShape.circle),
-                              ),
-                              const SizedBox(width: 6),
-                              Text(FlutterI18n.translate(context, 'online'),
-                                  style: FontTheme.of(context)
-                                      .h7()
-                                      .copyWith(fontWeight: FontWeight.w500)),
-                            ],
-                          ),
-                        ),
-                        MXCDropDown<String>(
-                          itemList: [
-                            state.walletAddress != null
-                                ? Formatter.formatWalletAddress(
-                                    state.walletAddress!.hex)
-                                : "",
-                          ],
-                          onChanged: (String? newValue) {},
-                          selectedItem: state.walletAddress != null
-                              ? Formatter.formatWalletAddress(
-                                  state.walletAddress!.hex)
-                              : "",
-                          textStyle: FontTheme.of(context).h7().copyWith(
-                              fontSize: 16, fontWeight: FontWeight.w400),
-                          icon: Padding(
-                            padding: const EdgeInsetsDirectional.only(start: 0),
-                            child: Icon(
-                              Icons.arrow_drop_down_rounded,
-                              size: 32,
-                              color: ColorsTheme.of(context).purpleMain,
                             ),
-                          ),
+                          ],
                         ),
                       ],
                     ),
-                  ],
-                ),
-                backgroundColor: ColorsTheme.of(context).secondaryBackground,
-              ) : null,
+                    backgroundColor:
+                        ColorsTheme.of(context).secondaryBackground,
+                  )
+                : null,
         body: PresenterHooks(
           presenter: presenter,
           child: splashLinearBackground(
