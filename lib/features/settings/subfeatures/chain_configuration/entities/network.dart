e9e7e6324d3d9382d0d9cf4a9b321f7035104cf6
reasje <rezaaslejeddian@gmail.com>
2023-07-28 20:53:01 +0330

    feat: Added gasLimit property to network

--- lib/features/settings/subfeatures/chain_configuration/entities/network.dart
+++ lib/features/settings/subfeatures/chain_configuration/entities/network.dart
@@ -1,14 +1,15 @@
 class Network {
   const Network(
-      this.logo,
-      this.web3RpcHttpUrl,
-      this.web3RpcWebsocketUrl,
-      this.web3WebSocketUrl,
-      this.symbol,
-      this.explorerUrl,
-      this.enabled,
-      this.label,
-      this.chainId);
+      {required this.logo,
+      required this.web3RpcHttpUrl,
+      required this.web3RpcWebsocketUrl,
+      required this.web3WebSocketUrl,
+      required this.symbol,
+      required this.explorerUrl,
+      required this.enabled,
+      required this.label,
+      required this.chainId,
+      this.gasLimit});
 
   final String logo;
   final String web3RpcHttpUrl;
@@ -19,43 +20,94 @@ class Network {
   final bool enabled;
   final String label;
   final int chainId;
+  final int? gasLimit;
 
+  Network copyWith({
+    String? logo,
+    String? web3RpcHttpUrl,
+    String? web3RpcWebsocketUrl,
+    String? web3WebSocketUrl,
+    String? symbol,
+    String? explorerUrl,
+    bool? enabled,
+    String? label,
+    int? chainId,
+    int? gasLimit,
+  }) {
+    return Network(
+      logo: logo ?? this.logo,
+      web3RpcHttpUrl: web3RpcHttpUrl ?? this.web3RpcHttpUrl,
+      web3RpcWebsocketUrl: web3RpcWebsocketUrl ?? this.web3RpcWebsocketUrl,
+      web3WebSocketUrl: web3WebSocketUrl ?? this.web3WebSocketUrl,
+      symbol: symbol ?? this.symbol,
+      explorerUrl: explorerUrl ?? this.explorerUrl,
+      enabled: enabled ?? this.enabled,
+      label: label ?? this.label,
+      chainId: chainId ?? this.chainId,
+      gasLimit: gasLimit ?? this.gasLimit,
+    );
+  }
+
+  // This data will be initialized for the first time
   static List<Network> fixedNetworks() {
     return [
       const Network(
-          'assets/svg/networks/wannsee.svg',
-          'https://wannsee-rpc.mxc.com',
-          'wss://wannsee-rpc.mxc.com',
-          'wss://wannsee-explorer-v1.mxc.com/socket/v2/websocket?vsn=2.0.0',
-          'MXC',
-          'https://wannsee-explorer.mxc.com',
-          true,
-          'MXC Wannsee Testnet',
-          5167003),
+          logo: 'assets/svg/networks/wannsee.svg',
+          web3RpcHttpUrl: 'https://wannsee-rpc.mxc.com',
+          web3RpcWebsocketUrl: 'wss://wannsee-rpc.mxc.com',
+          web3WebSocketUrl:
+              'wss://wannsee-explorer-v1.mxc.com/socket/v2/websocket?vsn=2.0.0',
+          symbol: 'MXC',
+          explorerUrl: 'https://wannsee-explorer.mxc.com',
+          enabled: true,
+          label: 'MXC Wannsee Testnet',
+          chainId: 5167003,
+          gasLimit: null),
       const Network(
-          'assets/svg/networks/wannsee.svg',
-          'https://rpc.mxc.com',
-          'wss://rpc.mxc.com',
-          'wss://wannsee-explorer-v1.mxc.com/socket/v2/websocket?vsn=2.0.0',
-          'MXC',
-          'https://explorer.mxc.com/',
-          false,
-          'MXC zkEVM Mainnet',
-          18686),
-      const Network('assets/svg/networks/ethereum.svg', '', '', '', 'Eth',
-          'https://etherscan.io/', false, 'Ethereum Mainnet', 1),
-      const Network('assets/svg/networks/arbitrum.svg', '', '', '', 'Eth',
-          'https://arbiscan.io/', false, 'Arbitrum One', 42161),
+          logo: 'assets/svg/networks/wannsee.svg',
+          web3RpcHttpUrl: 'https://rpc.mxc.com',
+          web3RpcWebsocketUrl: 'wss://rpc.mxc.com',
+          web3WebSocketUrl:
+              'wss://wannsee-explorer-v1.mxc.com/socket/v2/websocket?vsn=2.0.0',
+          symbol: 'MXC',
+          explorerUrl: 'https://explorer.mxc.com/',
+          enabled: false,
+          label: 'MXC zkEVM Mainnet',
+          chainId: 18686,
+          gasLimit: null),
       const Network(
-          'assets/svg/networks/arbitrum.svg',
-          'https://rpc.mxc.com',
-          'https://rpc.mxc.com',
-          '',
-          'AGOR',
-          'https://goerli-rollup-explorer.arbitrum.io/',
-          false,
-          'Arbitrum Goerli',
-          421613)
+          logo: 'assets/svg/networks/ethereum.svg',
+          web3RpcHttpUrl: '',
+          web3RpcWebsocketUrl: '',
+          web3WebSocketUrl: '',
+          symbol: 'Eth',
+          explorerUrl: 'https://etherscan.io/',
+          enabled: false,
+          label: 'Ethereum Mainnet',
+          chainId: 1,
+          gasLimit: null),
+      const Network(
+          logo: 'assets/svg/networks/arbitrum.svg',
+          web3RpcHttpUrl: '',
+          web3RpcWebsocketUrl: '',
+          web3WebSocketUrl: '',
+          symbol: 'Eth',
+          explorerUrl: 'https://arbiscan.io/',
+          enabled: false,
+          label: 'Arbitrum One',
+          chainId: 42161,
+          gasLimit: null),
+      const Network(
+          logo: 'assets/svg/networks/arbitrum.svg',
+          web3RpcHttpUrl: 'https://rpc.mxc.com',
+          web3RpcWebsocketUrl: 'https://rpc.mxc.com',
+          web3WebSocketUrl: '',
+          symbol: 'AGOR',
+          explorerUrl: 'https://goerli-rollup-explorer.arbitrum.io/',
+          enabled: false,
+          label: 'Arbitrum Goerli',
+          chainId: 42161,
+          gasLimit: null)
     ];
   }
 }
