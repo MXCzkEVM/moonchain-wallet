f244310e2fa332158491b8c61ae1d6af5f22a31d
rd101rahul <65663039+rd101rahul@users.noreply.github.com>
2023-08-10 11:46:01 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,6 @@ import 'package:datadashwallet/common/common.dart';
 import 'package:datadashwallet/core/core.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import com.microsoft.appcenter.AppCenter;
-import com.microsoft.appcenter.analytics.Analytics;
-import com.microsoft.appcenter.crashes.Crashes;
 
 import 'app/app.dart';
 
@@ -26,13 +23,4 @@ void main() async {
       ),
     ),
   );
-  _startAppCenter();
-}
-
-Future<void> _startAppCenter() async {
-  String appSecret = "d6920fcc-d680-42a1-afaf-4cf292d223ac";
-  AppCenter.start(getApplication(), "d6920fcc-d680-42a1-afaf-4cf292d223ac", Analytics.class, Crashes.class);
-  await AppCenter.start(appSecret: appSecret, enableAnalytics: true);
-  await AppCenterAnalytics.setEnabled(true);
-  await AppCenterCrashes.setEnabled(true);
 }
