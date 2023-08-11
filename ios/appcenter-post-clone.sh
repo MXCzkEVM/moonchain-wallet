3c11511519b82afdaa21d449efb0bd3dd4b6bb66
Adam Zh <zh.bh@qq.com>
2023-08-11 18:44:42 +0800

    Update appcenter-post-clone.sh

--- ios/appcenter-post-clone.sh
+++ ios/appcenter-post-clone.sh
@@ -11,30 +11,15 @@ git clone -b stable https://github.com/flutter/flutter.git
 export PATH=`pwd`/flutter/bin:$PATH
 export PATH="$PATH":"$HOME/.pub-cache/bin"
 
+# Note: there is a bug with Flutter 3.10.0 when the whole app rebuilds when we open keyboard
+# version 3.7.12 => 4d9e56e694b656610ab87fcf2efbcd226e0ed8cf
+cd flutter
+git reset --hard 4d9e56e694b656610ab87fcf2efbcd226e0ed8cf
+cd ..
+
 flutter clean
 
 echo "Installed flutter to `pwd`/flutter"
-
-# configure Firebase project
-if [ ! -z "$FIREBASE_PROJECT" ]; then
-  npm install -g firebase-tools
-
-  dart pub global activate flutterfire_cli
-
-  flutterfire configure -y \
-    --android-package-name="com.mxc.datadashwallet" \
-    --ios-bundle-id="com.mxc.datadashwallet" \
-    --macos-bundle-id="com.mxc.datadashwallet" \
-    --platforms="ios,android" \
-    -p "$FIREBASE_PROJECT" \
-    -t "$FIREBASE_TOKEN"
-fi
-
-cat > ~/.netrc <<- EOM
-machine api.mapbox.com
-   login mapbox
-   password ${MAP_BOX_SECRET_KEY}
-EOM
 
 chmod 0600 ~/.netrc
 
