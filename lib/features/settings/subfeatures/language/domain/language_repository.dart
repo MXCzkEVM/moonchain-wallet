f13adeba717ac06de6b2dbf52912137b863532b5
Adam Zh <zh.bh@qq.com>
2023-08-11 18:11:08 +0800

    Update language_repository.dart

--- lib/features/settings/subfeatures/language/domain/language_repository.dart
+++ lib/features/settings/subfeatures/language/domain/language_repository.dart
@@ -4,21 +4,21 @@ import 'package:datadashwallet/core/core.dart';
 class LanguageRepository extends GlobalCacheRepository {
   final List<Language> supportedLocales = const [
     Language('en', 'English'),
+    Language('de', 'Deutsch'),
+    Language('nl', 'Nederlands'),
+    Language('tr', 'Türkçe'),
     Language('zh_CN', '简体中文'),
     Language('zh_TW', '繁体中文'),
-    Language('de', 'Deutsch'),
-    Language('tr', 'Türkçe'),
-    Language('ro', 'Română'),
-    Language('nl', 'Nederlands'),
-    Language('es', 'Español'),
-    Language('fr', 'Français'),
     Language('ko', '한국어'),
-    Language('ja', '日本語'),
-    Language('ru', 'Русский'),
-    Language('pt', 'Portugués'),
-    Language('id', 'Indonesio'),
-    Language('tl', 'Tagalog'),
-    Language('vi', 'Tiếng Việt'),
+    Language('ro', 'Română'),
+    // Language('es', 'Español'),
+    // Language('fr', 'Français'),
+    // Language('ja', '日本語'),
+    // Language('ru', 'Русский'),
+    // Language('pt', 'Portugués'),
+    // Language('id', 'Indonesio'),
+    // Language('tl', 'Tagalog'),
+    // Language('vi', 'Tiếng Việt'),
   ];
 
   @override
