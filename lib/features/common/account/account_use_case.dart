09ccffa9b874c50d81d3d55febddbe69020946eb
reasje <rezaaslejeddian@gmail.com>
2023-08-24 21:46:32 +0330

    fix: Accounts & account details update

--- lib/features/common/account/account_use_case.dart
+++ lib/features/common/account/account_use_case.dart
@@ -32,7 +32,7 @@ class AccountUseCase extends ReactiveUseCase {
     final items = _accountCacheRepository.accountItems;
     update(account, item);
     update(accounts, items);
-    getAccountMns(item);
+    getAccountsNames();
   }
 
   void changeAccount(Account item) {
@@ -50,15 +50,29 @@ class AccountUseCase extends ReactiveUseCase {
 
   void getAccountsNames() async {
     for (Account account in accounts.value) {
-      getAccountMns(account);
+      await getAccountMns(account);
     }
+    update(accounts, _accountCacheRepository.accountItems);
+    update(account, _accountCacheRepository.accountItem);
   }
 
-  void getAccountMns(Account item) async {
-    final result = await _repository.tokenContract.getName(item.address);
-    if (item.mns != result) {
-      item.mns = result;
-      _accountCacheRepository.updateAccount(item);
+  Future<bool> getAccountMns(Account item) async {
+    try {
+      final result = await _repository.tokenContract.getName(item.address);
+      if (item.mns != result) {
+        item.mns = result;
+        _accountCacheRepository.updateAccount(item);
+      }
+      return true;
+    } catch (e) {
+      if (e == 'RangeError: Value not in range: 32') {
+        // The username is empty
+        item.mns = null;
+        _accountCacheRepository.updateAccount(item);
+        return true;
+      } else {
+        return false;
+      }
     }
   }
 }
