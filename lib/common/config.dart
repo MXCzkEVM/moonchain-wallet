ebbc2730832973a76139a447832256a4c41230ef
reasje <rezaaslejeddian@gmail.com>
2023-09-08 14:28:12 +0330

    fix: Ethereum tx handling

--- lib/common/config.dart
+++ lib/common/config.dart
@@ -1,10 +1,19 @@
 class Config {
   static const int mxcMainnetChainId = 18686;
   static const int mxcTestnetChainId = 5167003;
+  static const int ethereumMainnetChainId = 1;
   static const int ethDecimals = 18;
   static const String mxcSymbol = 'MXC';
   static const String mxcName = 'MXC Token';
   static const String mxcLogoUri = 'assets/svg/networks/mxc.svg';
   static const String zeroAddress =
       '0x0000000000000000000000000000000000000000';
+  
+  static bool isMxcChains(int chainId) {
+    return chainId == mxcMainnetChainId || chainId == mxcTestnetChainId;
+  }
+
+  static bool isEthereumMainnet(int chainId) {
+    return chainId == ethereumMainnetChainId;
+  }
 }
