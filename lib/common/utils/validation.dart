be28a97b7d7ac54be8d0d2fcd31408645e0d1305
reasje <rezaaslejeddian@gmail.com>
2023-09-13 11:03:42 +0330

    fix: Added expo number validation

--- lib/common/utils/validation.dart
+++ lib/common/utils/validation.dart
@@ -83,4 +83,9 @@ class Validation {
       return null;
     }
   }
+
+  static bool isExpoNumber(String input) {
+    RegExp regex = RegExp(r'^(\d+\.\d+e[-+]\d+)$');
+    return regex.hasMatch(input);
+  }
 }
