2cf1d2e1b2d4495929399d4f6f4047d28e14d880
reasje <rezaaslejeddian@gmail.com>
2023-10-19 10:39:10 +0330

    feat: Added utils with email app launch & check

--- lib/common/utils/utils.dart
+++ lib/common/utils/utils.dart
@@ -1,3 +1,24 @@
+import 'package:datadashwallet/common/common.dart';
+import 'package:url_launcher/url_launcher.dart';
+
 export 'formatter.dart';
 export 'permission.dart';
 export 'validation.dart';
+
+class Utils {
+  static Future<bool> isEmailAppAvailable() async {
+    final url = Uri.parse(Urls.emailApp);
+
+    return await canLaunchUrl(url);
+  }
+
+  static Future<void> launchEmailApp() async {
+    final url = Uri.parse(Urls.emailApp);
+
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'unable_to_launch_email_app';
+    }
+  }
+}
