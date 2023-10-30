352fb2952e3804ae0470b3072fbf04205daef55a
reasje <rezaaslejeddian@gmail.com>
2023-10-30 16:31:02 +0330

    feat: Added receive bottom sheet to mns register section

--- lib/features/splash/mns_process/presentation/mns_query/mns_query_presenter.dart
+++ lib/features/splash/mns_process/presentation/mns_query/mns_query_presenter.dart
@@ -1,7 +1,8 @@
+import 'package:datadashwallet/common/common.dart';
 import 'package:datadashwallet/common/config.dart';
 import 'package:datadashwallet/features/dapps/dapps.dart';
 import 'package:datadashwallet/core/core.dart';
-import 'package:datadashwallet/features/portfolio/presentation/widgets/show_wallet_address_dialog.dart';
+import 'package:datadashwallet/common/dialogs/wallet_address_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/src/consumer.dart';
 import 'mns_query_state.dart';
@@ -76,10 +77,30 @@ class SplashMNSQueryPresenter extends CompletePresenter<SplashMNSQueryState> {
         final result = await showNoBalanceDialog(context!);
         if (result != null) {
           if (result) {
-            showWalletAddressDialog(
-              context: context!,
-              walletAddress: state.walletAddress,
-            );
+            final walletAddress = state.walletAddress!;
+            if (Config.isMxcChains(state.network!.chainId)) {
+              showWalletAddressDialogMXCChains(
+                  context: context!,
+                  walletAddress: walletAddress,
+                  onL3Tap: () {
+                    final chainId = state.network!.chainId;
+                    final l3BridgeUri = Urls.networkL3Bridge(chainId);
+                    Navigator.of(context!).push(route.featureDialog(
+                      maintainState: false,
+                      OpenAppPage(
+                        url: l3BridgeUri,
+                      ),
+                    ));
+                  },
+                  launchUrlInPlatformDefault:
+                      _chainConfigurationUseCase.launchUrlInPlatformDefault);
+            } else {
+              final networkSymbol = state.network!.symbol;
+              showWalletAddressDialogOtherChains(
+                  context: context!,
+                  walletAddress: walletAddress,
+                  networkSymbol: networkSymbol);
+            }
           } else {
             navigator?.replaceAll(route(const DAppsPage()));
           }
