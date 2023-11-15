c09d949652b72be2e7f1bb4ed552f880b368f422
reasje <rezaaslejeddian@gmail.com>
2023-11-15 10:26:57 +0330

    imprv: Removed url_launcher util & cleaned utils

--- lib/common/utils/utils.dart
+++ lib/common/utils/utils.dart
@@ -7,22 +7,6 @@ export 'permission.dart';
 export 'validation.dart';
 
 class Utils {
-  static Future<bool> isEmailAppAvailable() async {
-    final url = Uri.parse(Urls.emailApp);
-
-    return await canLaunchUrl(url);
-  }
-
-  static Future<void> launchEmailApp() async {
-    final url = Uri.parse(Urls.emailApp);
-
-    if (await canLaunchUrl(url)) {
-      await launchUrl(url);
-    } else {
-      throw 'unable_to_launch_email_app';
-    }
-  }
-
   static void retryFunction(Function function) {
     retry(() => function());
   }
