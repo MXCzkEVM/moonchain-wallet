b39fe03e94286c32772d4454ead451c20f30e0f2
reasje <rezaaslejeddian@gmail.com>
2023-11-20 13:23:35 +0330

    feat: Added firebase initialization in app

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,12 @@ import 'dart:async';
 import 'package:datadashwallet/app/logger.dart';
 import 'package:datadashwallet/common/common.dart';
 import 'package:datadashwallet/core/core.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mxc_logic/mxc_logic.dart';
 
 import 'app/app.dart';
 
@@ -19,6 +22,13 @@ void main() {
   runZoned(
     () async {
       WidgetsFlutterBinding.ensureInitialized();
+
+      await Firebase.initializeApp(
+        name: Config.appName,
+        options: AXSFireBase.defaultOptions,
+      );
+      FirebaseMessaging.onBackgroundMessage(
+          AXSFireBase.firebaseMessagingBackgroundHandler);
 
       await dotenv.load(fileName: 'assets/.env');
       await initLogs();
@@ -35,6 +45,8 @@ void main() {
 
       final initializationUseCase = container.read(chainsUseCaseProvider);
       initializationUseCase.updateChains();
+
+      AXSFireBase.initLocalNotificationsAndInteractions();
 
       runApp(
         UncontrolledProviderScope(
