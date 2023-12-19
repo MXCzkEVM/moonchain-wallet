ddb508341e24bd63bce18df6f6a7bf27eb1db8ba
reasje <rezaaslejeddian@gmail.com>
2023-12-19 10:56:36 +0330

    imprv: drag down panel ui update

--- lib/features/dapps/subfeatures/open_dapp/widgets/drag_down_panel.dart
+++ lib/features/dapps/subfeatures/open_dapp/widgets/drag_down_panel.dart
@@ -1,6 +1,8 @@
+import 'package:datadashwallet/features/settings/subfeatures/chain_configuration/widgets/chain_logo_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mxc_ui/mxc_ui.dart';
 
 import '../open_dapp_presenter.dart';
 
@@ -19,6 +21,11 @@ class DragDownPanel extends HookConsumerWidget {
     final presenter = ref.read(openDAppPageContainer.actions);
     final state = ref.watch(openDAppPageContainer.state);
 
+    final logo = state.network!.logo;
+    final name = state.network!.label ?? state.network!.explorerUrl;
+    final url = state.currentUrl;
+    final isSecure = state.isSecure;
+
     state.animationController = useAnimationController();
 
     return AnimatedBuilder(
@@ -29,14 +36,63 @@ class DragDownPanel extends HookConsumerWidget {
           clipBehavior: Clip.hardEdge,
           children: [
             Container(
-              // duration: Duration(milliseconds: 100),
-              height: maxPanelHeight  * state.animationController!.value,
-              // duration: const Duration(seconds: 2),
+              height: maxPanelHeight * state.animationController!.value / 1.5,
               width: double.infinity,
-              color: Colors.amber,
+              padding: const EdgeInsets.symmetric(
+                  horizontal: Sizes.spaceSmall, vertical: 10),
+              color: ColorsTheme.of(context).primaryBackground,
+              child: Container(
+                height: double.infinity,
+                width: double.infinity,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 2),
+                decoration: BoxDecoration(
+                    color: ColorsTheme.of(context).screenBackground,
+                    borderRadius: const BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                    border: Border.all(
+                      color: ColorsTheme.of(context).screenBackground,
+                    )),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ChainLogoWidget(logo: logo),
+                    Row(
+                      children: [
+                        isSecure
+                            ? Icon(
+                                Icons.lock_rounded,
+                                color: ColorsTheme.of(context).textPrimary,
+                                size: 24,
+                              )
+                            : Icon(
+                                Icons.lock_open,
+                                color: ColorsTheme.of(context).mainRed,
+                                size: 24,
+                              ),
+                        const SizedBox(
+                          width: Sizes.spaceXSmall,
+                        ),
+                        Text(
+                          url?.host ?? '',
+                          style: FontTheme.of(context).body1.primary(),
+                        )
+                      ],
+                    ),
+                    IconButton(
+                        key: const Key('closedAppButton'),
+                        onPressed: () {
+                          presenter.closedApp();
+                        },
+                        icon: const Icon(Icons.close_rounded))
+                  ],
+                ),
+              ),
             ),
             Transform.translate(
-              offset: Offset(0.0, maxPanelHeight  *  state.animationController!.value),
+              offset: Offset(
+                  0.0, maxPanelHeight * state.animationController!.value / 1.5),
               child: child,
             ),
           ],
