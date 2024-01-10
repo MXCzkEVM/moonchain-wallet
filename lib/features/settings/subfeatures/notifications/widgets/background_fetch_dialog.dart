dae356cf0a16956090ead81b783084d0de8d679f
reasje <rezaaslejeddian@gmail.com>
2024-01-10 18:38:24 +0330

    imprv: background fetch dialog updated

--- lib/features/settings/subfeatures/notifications/widgets/background_fetch_dialog.dart
+++ lib/features/settings/subfeatures/notifications/widgets/background_fetch_dialog.dart
@@ -5,52 +5,63 @@ import 'package:mxc_ui/mxc_ui.dart';
 Future<bool?> showBackgroundFetchAlertDialog({
   required BuildContext context,
 }) async {
-  return showDialog<bool>(
+  String translate(String text) => FlutterI18n.translate(context, text);
+
+  return showModalBottomSheet<bool>(
     context: context,
-    barrierDismissible: false,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(20)),
+    useRootNavigator: true,
+    isScrollControlled: true,
+    backgroundColor: Colors.transparent,
+    useSafeArea: true,
+    builder: (BuildContext context) => Container(
+      padding: const EdgeInsets.only(
+          top: Sizes.spaceNormal, bottom: Sizes.space3XLarge),
+      decoration: BoxDecoration(
+        color: ColorsTheme.of(context).layerSheetBackground,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
         ),
-        backgroundColor: ColorsTheme.of(context).cardBackground,
-        title: Text(
-          FlutterI18n.translate(context, 'background_fetch_notice_title'),
-          style: FontTheme.of(context).h6().copyWith(
-                fontWeight: FontWeight.w500,
-              ),
-          textAlign: TextAlign.center,
-        ),
-        contentPadding: const EdgeInsets.only(
-          bottom: Sizes.space2XLarge,
-          left: Sizes.spaceNormal,
-          right: Sizes.spaceNormal,
-        ),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              FlutterI18n.translate(context, 'background_fetch_notice_text'),
-              style: FontTheme.of(context).body1(),
-              textAlign: TextAlign.center,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          MxcAppBarEvenly.title(
+            titleText: translate('background_fetch_notice_title'),
+            useContentPadding: false,
+            textFieldFlex: 5,
+          ),
+          Padding(
+            padding: const EdgeInsetsDirectional.symmetric(
+              horizontal: Sizes.spaceXLarge,
             ),
-            const SizedBox(height: Sizes.spaceXLarge),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+            child: Column(
               children: [
-                MxcButton.primaryWarning(
+                Text(
+                  translate(
+                    'background_fetch_notice_text',
+                  ),
+                  style: FontTheme.of(context).body1.primary().copyWith(),
+                  softWrap: true,
+                  textAlign: TextAlign.justify,
+                ),
+                const SizedBox(
+                  height: Sizes.spaceNormal,
+                ),
+                MxcButton.primary(
                   key: const ValueKey('acknowledgeButton'),
-                  title: FlutterI18n.translate(context, 'acknowledge'),
-                  titleColor: ColorsTheme.of(context).textBlack200,
+                  title: translate('acknowledge'),
+                  onTap: () {
+                    Navigator.of(context).pop(true);
+                  },
                   size: AxsButtonSize.xl,
-                  width: 120,
-                  onTap: () => Navigator.of(context).pop(true),
                 ),
               ],
             ),
-          ],
-        ),
-      );
-    },
+          )
+        ],
+      ),
+    ),
   );
 }
