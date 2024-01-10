0e37b70e8a4ceff5c8cc0833192a15eedeef1c0f
reasje <rezaaslejeddian@gmail.com>
2024-01-10 20:49:15 +0330

    feat: notifications init implementation

--- lib/features/settings/subfeatures/notifications/notificaitons_page.dart
+++ lib/features/settings/subfeatures/notifications/notificaitons_page.dart
@@ -1,12 +1,15 @@
 import 'package:datadashwallet/common/common.dart';
 import 'package:datadashwallet/features/common/common.dart';
+import 'package:datadashwallet/features/settings/subfeatures/notifications/widgets/epoch_occur_dialog.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mxc_logic/mxc_logic.dart';
 import 'package:mxc_ui/mxc_ui.dart';
 
 import 'notifications_presenter.dart';
 import 'notifications_state.dart';
+import 'widgets/switch_row_item.dart';
 
 class NotificationsPage extends HookConsumerWidget {
   const NotificationsPage({Key? key}) : super(key: key);
@@ -23,53 +26,176 @@ class NotificationsPage extends HookConsumerWidget {
     final notificationsState = ref.watch(state);
     final notificationsPresenter = ref.read(presenter);
 
+    final expectedEpochOccur =
+        notificationsState.periodicalCallData!.expectedEpochOccurrence;
+
     String translate(String text) => FlutterI18n.translate(context, text);
 
-    return MxcPage(
-      presenter: ref.watch(presenter),
-      crossAxisAlignment: CrossAxisAlignment.start,
-      appBar: AppNavBar(
-        title: Text(
-          FlutterI18n.translate(context, 'notifications'),
-          style: FontTheme.of(context).body1.primary(),
+    return Form(
+      key: notificationsState.formKey,
+      child: MxcPage(
+        presenter: ref.watch(presenter),
+        crossAxisAlignment: CrossAxisAlignment.start,
+        appBar: AppNavBar(
+          title: Text(
+            FlutterI18n.translate(context, 'notifications'),
+            style: FontTheme.of(context).body1.primary(),
+          ),
         ),
+        children: [
+          SwitchRowItem(
+            title: translate('notifications'),
+            value: notificationsState.isNotificationsEnabled,
+            onChanged: notificationsPresenter.changeNotificationsState,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: Sizes.spaceNormal),
+              Text(
+                translate('why_enable_notifications'),
+                style: FontTheme.of(context).subtitle1.primary(),
+                textAlign: TextAlign.start,
+              ),
+              const SizedBox(height: Sizes.spaceNormal),
+              Text(
+                translate('why_enable_notifications_notice'),
+                style: FontTheme.of(context).subtitle1.secondary(),
+                textAlign: TextAlign.justify,
+              ),
+            ],
+          ),
+          const SizedBox(height: Sizes.spaceNormal),
+          Text(
+            translate('background_notifications_frequency'),
+            style: FontTheme.of(context).body2.primary(),
+          ),
+          const SizedBox(height: Sizes.spaceNormal),
+          MXCDropDown(
+            key: const Key('bgNotificationsFrequencyDropDown'),
+            onTap: () {
+              showEpochOccurDialog(context,
+                  onTap: notificationsPresenter.selectEpochOccur,
+                  selectedOccur: notificationsState
+                      .periodicalCallData!.expectedEpochOccurrence);
+            },
+            selectedItem: '$expectedEpochOccur  Epoch occurrence',
+          ),
+          const SizedBox(height: Sizes.spaceNormal),
+          SwitchRowItem(
+            title: translate('low_balance'),
+            value:
+                notificationsState.periodicalCallData!.lowBalanceLimitEnabled,
+            onChanged: notificationsPresenter.enableLowBalanceLimit,
+          ),
+          MxcTextField(
+            key: const ValueKey('lowBalanceTextField'),
+            hint: 'e.g 300',
+            controller: ref.read(presenter).lowBalanceController,
+            keyboardType: TextInputType.number,
+            action: TextInputAction.next,
+            suffixText: ref.watch(state).network!.symbol,
+            // readOnly: !notificationsState.periodicalCallData!.lowBalanceLimitEnabled
+            validator: (value) {
+              value = ref.read(presenter).lowBalanceController.text;
+              final res = Validation.notEmpty(
+                  context,
+                  value,
+                  translate('x_not_empty')
+                      .replaceFirst('{0}', translate('amount')));
+              if (res != null) {
+                return res;
+              }
+              try {
+                final doubleValue = double.parse(value);
+                String stringValue = doubleValue.toString();
+
+                int decimalPlaces = stringValue.split('.')[1].length;
+
+                if (doubleValue.isNegative ||
+                    decimalPlaces > Config.decimalWriteFixed) {
+                  return translate('invalid_format');
+                }
+                return null;
+              } catch (e) {
+                return translate('invalid_format');
+              }
+            },
+            // onChanged: (value) {
+            //   if (!notificationsState.formKey.currentState!.validate()) return;
+            // },
+            onFocused: (focused) => focused
+                ? null
+                : notificationsState.formKey.currentState!.validate(),
+          ),
+          const SizedBox(height: Sizes.spaceNormal),
+          SwitchRowItem(
+            title: translate('expected_gas_price'),
+            value:
+                notificationsState.periodicalCallData!.expectedGasPriceEnabled,
+            onChanged: notificationsPresenter.enableExpectedGasPrice,
+          ),
+          // MxcTextField(
+          //   key: const ValueKey('addressTextField'),
+          //   label: '${translate('token_contract_addresss')} *',
+          //   hint: translate('enter_x').replaceFirst(
+          //       '{0}', translate('token_contract_addresss').toLowerCase()),
+          //   controller: ref.read(presenter).addressController,
+          //   action: TextInputAction.done,
+          //   validator: (value) {
+          //     final res = Validation.notEmpty(
+          //         context,
+          //         value,
+          //         translate('x_not_empty')
+          //             .replaceFirst('{0}', translate('token_contract_addresss')));
+          //     if (res != null) return res;
+
+          //     return Validation.checkEthereumAddress(context, value!);
+          //   },
+          //   onChanged: (value) {
+          //     if (!formKey.currentState!.validate()) return;
+          //     ref.read(presenter).onChanged(value);
+          //   },
+          //   onFocused: (focused) =>
+          //       focused ? null : formKey.currentState!.validate(),
+          // ),
+          const SizedBox(height: Sizes.spaceNormal),
+          SwitchRowItem(
+            title: translate('expected_epoch_occur'),
+            value: notificationsState
+                .periodicalCallData!.expectedEpochOccurrenceEnabled,
+            onChanged: notificationsPresenter.enableExpectedEpochQuantity,
+          ),
+          const SizedBox(height: Sizes.spaceNormal),
+          MXCDropDown(
+            key: const Key('epochOccurrenceDropDown'),
+            onTap: () {
+              showEpochOccurDialog(context,
+                  onTap: notificationsPresenter.selectEpochOccur,
+                  selectedOccur: notificationsState
+                      .periodicalCallData!.expectedEpochOccurrence);
+            },
+            selectedItem: '$expectedEpochOccur  Epoch occurrence',
+          ),
+          // Row(
+          //   children: [
+          //     Text(
+          //       translate('notifications'),
+          //       style: FontTheme.of(context).body2.primary(),
+          //     ),
+          //     const Spacer(),
+          //     const SizedBox(
+          //       width: 16,
+          //     ),
+          //     CupertinoSwitch(
+          //       value: notificationsState.isNotificationsEnabled,
+          //       onChanged: (value) =>
+          //           notificationsPresenter.changeNotificationsState(value),
+          //     ),
+          //   ],
+          // ),
+        ],
       ),
-      children: [
-        Row(
-          children: [
-            Text(
-              translate('notifications'),
-              style: FontTheme.of(context).body2.primary(),
-            ),
-            const Spacer(),
-            const SizedBox(
-              width: 16,
-            ),
-            CupertinoSwitch(
-              value: notificationsState.isNotificationsEnabled,
-              onChanged: (value) =>
-                  notificationsPresenter.changeNotificationsState(value),
-            ),
-          ],
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: Sizes.spaceNormal),
-            Text(
-              translate('why_enable_notifications'),
-              style: FontTheme.of(context).subtitle1.primary(),
-              textAlign: TextAlign.start,
-            ),
-            const SizedBox(height: Sizes.spaceNormal),
-            Text(
-              translate('why_enable_notifications_notice'),
-              style: FontTheme.of(context).subtitle1.secondary(),
-              textAlign: TextAlign.justify,
-            ),
-          ],
-        )
-      ],
     );
   }
 }
--- lib/features/settings/subfeatures/notifications/notifications_presenter.dart
+++ lib/features/settings/subfeatures/notifications/notifications_presenter.dart
@@ -1,8 +1,13 @@
 import 'dart:io';
 import 'package:app_settings/app_settings.dart';
 import 'package:datadashwallet/common/common.dart';
+import 'package:datadashwallet/features/settings/subfeatures/notifications/widgets/background_fetch_dialog.dart';
+import 'package:datadashwallet/features/settings/subfeatures/notifications/widgets/bg_notifications_frequency_dialog.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:datadashwallet/core/core.dart';
+import 'package:background_fetch/background_fetch.dart' as bgFetch;
+import 'package:mxc_logic/mxc_logic.dart';
+import '../../../../main.dart';
 import 'notifications_state.dart';
 
 final notificationsContainer =
@@ -15,10 +20,41 @@ class NotificationsPresenter extends CompletePresenter<NotificationsState>
     WidgetsBinding.instance.addObserver(this);
   }
 
+  late final backgroundFetchConfigUseCase =
+      ref.read(backgroundFetchConfigUseCaseProvider);
+  late final _chainConfigurationUseCase =
+      ref.read(chainConfigurationUseCaseProvider);
+
+  // this is used to show the bg fetch dialog
+  bool noneEnabled = true;
+
+  final TextEditingController lowBalanceController = TextEditingController();
+  final TextEditingController transactionFeeController =
+      TextEditingController();
+
   @override
   void initState() {
     super.initState();
     checkNotificationsStatus();
+
+    listen(backgroundFetchConfigUseCase.periodicalCallData, (value) {
+      checkPeriodicalCallDataChange(value);
+    });
+
+    listen(_chainConfigurationUseCase.selectedNetwork, (value) {
+      notify(() => state.network = value);
+    });
+
+    lowBalanceController.addListener(onLowBalanceChange);
+    transactionFeeController.addListener(onTransactionFeeChange);
+  }
+
+  void onLowBalanceChange() {
+    state.formKey.currentState!.validate();
+  }
+
+  void onTransactionFeeChange() {
+    state.formKey.currentState!.validate();
   }
 
   @override
@@ -72,8 +108,164 @@ class NotificationsPresenter extends CompletePresenter<NotificationsState>
     if (state.isNotificationsEnabled == false && isGranted == true) {
       await AXSFireBase.initializeFirebase();
       AXSFireBase.initLocalNotificationsAndListeners();
-    } 
+    }
     notify(() => state.isNotificationsEnabled = isGranted);
+  }
+
+  void enableLowBalanceLimit(bool value) {
+    // showBackgroundFetchAlertDialog(context: context!);
+    showBGNotificationsFrequencyDialog(context!,
+        onTap: handleFrequencyChange,
+        selectedFrequency: getPeriodicalCallDurationFromInt(
+            state.periodicalCallData!.duration));
+    // final newPeriodicalCallData =
+    //     state.periodicalCallData!.copyWith(lowBalanceLimitEnabled: value);
+    // backgroundFetchConfigUseCase.updateItem(newPeriodicalCallData);
+  }
+
+  void enableExpectedGasPrice(bool value) {
+    final newPeriodicalCallData =
+        state.periodicalCallData!.copyWith(expectedGasPriceEnabled: value);
+    backgroundFetchConfigUseCase.updateItem(newPeriodicalCallData);
+  }
+
+  void enableExpectedEpochQuantity(bool value) {
+    final newPeriodicalCallData = state.periodicalCallData!
+        .copyWith(expectedEpochOccurrenceEnabled: value);
+    backgroundFetchConfigUseCase.updateItem(newPeriodicalCallData);
+  }
+
+  void selectEpochOccur(int value) {
+    final newPeriodicalCallData =
+        state.periodicalCallData!.copyWith(expectedEpochOccurrence: value);
+    backgroundFetchConfigUseCase.updateItem(newPeriodicalCallData);
+  }
+
+  void handleFrequencyChange(PeriodicalCallDuration duration) {
+    final newPeriodicalCallData =
+        state.periodicalCallData!.copyWith(duration: duration.toMinutes());
+    backgroundFetchConfigUseCase.updateItem(newPeriodicalCallData);
+  }
+
+  void checkPeriodicalCallDataChange(
+      PeriodicalCallData newPeriodicalCallData) async {
+    bool newNoneEnabled =
+        !(newPeriodicalCallData.expectedEpochOccurrenceEnabled ||
+            newPeriodicalCallData.expectedGasPriceEnabled ||
+            newPeriodicalCallData.lowBalanceLimitEnabled);
+
+    if (state.periodicalCallData != null) {
+      if (isServicesEnabledStatusChanged(
+                  newPeriodicalCallData, state.periodicalCallData!) &&
+              hasAnyServiceBeenEnabled(
+                  newPeriodicalCallData, state.periodicalCallData!) ||
+          hasDurationChanged(
+              newPeriodicalCallData, state.periodicalCallData!)) {}
+
+      // none enabled means stopped || was stopped
+      if (newNoneEnabled == true) {
+        await stopBGFetch();
+      }
+      // If none was enabled & now one is enabled => Start BG service
+      // Other wise It was enabled so start BG service in case It's not running
+      else if (noneEnabled == true && newNoneEnabled == false) {
+        startBGFetch(newPeriodicalCallData.duration);
+      }
+    }
+    noneEnabled = newNoneEnabled;
+    notify(() => state.periodicalCallData = newPeriodicalCallData);
+  }
+
+  // Detect If change was about service enable status not amount change because amount changes won't effect the service & will be loaded from DB.
+  bool isServicesEnabledStatusChanged(PeriodicalCallData newPeriodicalCallData,
+      PeriodicalCallData periodicalCallData) {
+    return newPeriodicalCallData.expectedGasPriceEnabled !=
+            periodicalCallData.expectedGasPriceEnabled ||
+        newPeriodicalCallData.lowBalanceLimitEnabled !=
+            periodicalCallData.lowBalanceLimitEnabled ||
+        newPeriodicalCallData.expectedEpochOccurrenceEnabled !=
+            periodicalCallData.expectedEpochOccurrenceEnabled;
+  }
+
+  // There is a chance where user disables any service so in this case we don't want to run BG fetch service init again.
+  bool hasAnyServiceBeenEnabled(PeriodicalCallData newPeriodicalCallData,
+      PeriodicalCallData periodicalCallData) {
+    return (newPeriodicalCallData.expectedGasPriceEnabled == true &&
+            periodicalCallData.expectedGasPriceEnabled == false) &&
+        (newPeriodicalCallData.lowBalanceLimitEnabled == true &&
+            periodicalCallData.lowBalanceLimitEnabled == false) &&
+        (newPeriodicalCallData.expectedEpochOccurrenceEnabled == true &&
+            periodicalCallData.expectedEpochOccurrenceEnabled == false);
+  }
+
+  bool hasDurationChanged(PeriodicalCallData newPeriodicalCallData,
+      PeriodicalCallData periodicalCallData) {
+    return newPeriodicalCallData.duration != periodicalCallData.duration;
+  }
+
+  // delay is in minutes
+  void startBGFetch(int delay) async {
+    try {
+      // Stop If any is running
+      await stopBGFetch();
+
+      final configurationState = await bgFetch.BackgroundFetch.configure(
+          bgFetch.BackgroundFetchConfig(
+              minimumFetchInterval: 15,
+              stopOnTerminate: false,
+              enableHeadless: true,
+              startOnBoot: true,
+              requiresBatteryNotLow: false,
+              requiresCharging: false,
+              requiresStorageNotLow: false,
+              requiresDeviceIdle: false,
+              requiredNetworkType: bgFetch.NetworkType.ANY),
+          callbackDispatcherForeGround);
+      // Android Only
+      final backgroundFetchState =
+          await bgFetch.BackgroundFetch.registerHeadlessTask(
+              callbackDispatcher);
+
+      final scheduleState =
+          await bgFetch.BackgroundFetch.scheduleTask(bgFetch.TaskConfig(
+        taskId: Config.axsPeriodicalTask,
+        delay: delay * 60 * 1000,
+        periodic: true,
+        requiresNetworkConnectivity: true,
+        startOnBoot: true,
+        stopOnTerminate: false,
+        requiredNetworkType: bgFetch.NetworkType.ANY,
+      ));
+
+      if (scheduleState &&
+              configurationState == bgFetch.BackgroundFetch.STATUS_AVAILABLE ||
+          configurationState == bgFetch.BackgroundFetch.STATUS_RESTRICTED &&
+              (Platform.isAndroid ? backgroundFetchState : true)) {
+        showBGFetchSuccessSnackBar();
+      } else {
+        showBGFetchFailureSnackBar();
+      }
+    } catch (e) {
+      showBGFetchFailureSnackBar();
+    }
+  }
+
+  Future<int> stopBGFetch() async {
+    return await bgFetch.BackgroundFetch.stop(Config.axsPeriodicalTask);
+  }
+
+  void showBGFetchFailureSnackBar() {
+    showSnackBar(
+        context: context!,
+        content: translate('unable_to_launch_background_notification_service')!,
+        type: SnackBarType.fail);
+  }
+
+  void showBGFetchSuccessSnackBar() {
+    showSnackBar(
+        context: context!,
+        content: translate(
+            'Background_notifications_service_launched_successfully')!);
   }
 
   @override
