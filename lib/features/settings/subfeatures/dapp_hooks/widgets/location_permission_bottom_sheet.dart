17c92d9dde4bb85ae2341c5b22ed40e60aec7dbb
reasje <rezaaslejeddian@gmail.com>
2024-01-26 13:06:28 +0330

    feat: location permission bottom sheet

---
+++ lib/features/settings/subfeatures/dapp_hooks/widgets/location_permission_bottom_sheet.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:mxc_ui/mxc_ui.dart';
+
+Future<bool?> showLocationPermissionBottomSheet({
+  required BuildContext context,
+  required Function openLocationSettings,
+}) async {
+  String translate(String text) => FlutterI18n.translate(context, text);
+
+  return showModalBottomSheet<bool>(
+    context: context,
+    useRootNavigator: true,
+    isScrollControlled: true,
+    backgroundColor: Colors.transparent,
+    useSafeArea: true,
+    builder: (BuildContext context) => Container(
+      padding: const EdgeInsets.only(
+          top: Sizes.spaceNormal, bottom: Sizes.space3XLarge),
+      decoration: BoxDecoration(
+        color: ColorsTheme.of(context).layerSheetBackground,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          MxcAppBarEvenly.title(
+            titleText: translate('location_permission_required_title'),
+            useContentPadding: false,
+            textFieldFlex: 5,
+          ),
+          Padding(
+            padding: const EdgeInsetsDirectional.symmetric(
+              horizontal: Sizes.spaceXLarge,
+            ),
+            child: Column(
+              children: [
+                Text(
+                  translate(
+                    'location_permission_required_text',
+                  ),
+                  style: FontTheme.of(context).body1.primary().copyWith(),
+                  softWrap: true,
+                  textAlign: TextAlign.justify,
+                ),
+                const SizedBox(
+                  height: Sizes.spaceNormal,
+                ),
+                MxcButton.primary(
+                  key: const ValueKey('openLocationSettingsButton'),
+                  title: translate('open_settings'),
+                  onTap: () {
+                    openLocationSettings();
+                    Navigator.of(context).pop(true);
+                  },
+                  size: AxsButtonSize.xl,
+                ),
+                const SizedBox(
+                  height: Sizes.spaceNormal,
+                ),
+                MxcButton.secondary(
+                  key: const ValueKey('cancelButton'),
+                  title: translate('cancel'),
+                  onTap: () {
+                    Navigator.of(context).pop(true);
+                  },
+                  size: AxsButtonSize.xl,
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ),
+  );
+}
