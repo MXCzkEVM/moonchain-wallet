5a7a9cf79ba42aa8ff4fe06de94785f1cfe860cc
reasje <rezaaslejeddian@gmail.com>
2024-02-01 12:06:35 +0330

    refactor: more customization on switch row widget

--- lib/features/settings/subfeatures/notifications/widgets/switch_row_item.dart
+++ lib/features/settings/subfeatures/notifications/widgets/switch_row_item.dart
@@ -9,37 +9,45 @@ class SwitchRowItem extends StatelessWidget {
   final void Function(bool)? onChanged;
   final bool enabled;
   final Widget? textTrailingWidget;
+  final EdgeInsets? paddings;
+  final Color? switchActiveColor;
   const SwitchRowItem(
       {super.key,
       required this.title,
       required this.value,
       this.onChanged,
       required this.enabled,
-      this.textTrailingWidget});
+      this.textTrailingWidget,
+      this.paddings,
+      this.switchActiveColor});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text(
-          title,
-          style: FontTheme.of(context).body2.primary(),
-        ),
-        if (textTrailingWidget != null) ...[
-          const SizedBox(
-            width: Sizes.spaceXSmall,
+    return Padding(
+      padding: paddings ?? EdgeInsets.zero,
+      child: Row(
+        children: [
+          Text(
+            title,
+            style: FontTheme.of(context).body2.primary(),
           ),
-          textTrailingWidget!
+          if (textTrailingWidget != null) ...[
+            const SizedBox(
+              width: Sizes.spaceXSmall,
+            ),
+            textTrailingWidget!
+          ],
+          const Spacer(),
+          const SizedBox(
+            width: Sizes.spaceNormal,
+          ),
+          CupertinoSwitch(
+            value: value,
+            onChanged: enabled ? onChanged : null,
+            activeColor: switchActiveColor,
+          ),
         ],
-        const Spacer(),
-        const SizedBox(
-          width: Sizes.spaceNormal,
-        ),
-        CupertinoSwitch(
-          value: value,
-          onChanged: enabled ? onChanged : null,
-        ),
-      ],
+      ),
     );
   }
 }
