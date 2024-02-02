87df8c3bc48bf6efc64efa3d1a553c158e37084c
reasje <rezaaslejeddian@gmail.com>
2024-02-02 10:45:18 +0330

    refactor info widget text dynamic

--- lib/features/settings/subfeatures/dapp_hooks/widgets/dapp_hooks_information_widget.dart
+++ lib/features/settings/subfeatures/dapp_hooks/widgets/dapp_hooks_information_widget.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:mxc_ui/mxc_ui.dart';
 
 class DAppHooksInformation extends StatelessWidget {
-  const DAppHooksInformation({super.key});
+  final List<InlineSpan> texts; 
+  const DAppHooksInformation({
+    Key? key,
+    required this.texts,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,22 +21,7 @@ class DAppHooksInformation extends StatelessWidget {
           style: FontTheme.of(context)
               .subtitle1()
               .copyWith(color: ColorsTheme.of(context).chipTextBlack),
-          children: [
-            TextSpan(
-              text: FlutterI18n.translate(context, 'experiencing_issues'),
-              style: FontTheme.of(context)
-                  .subtitle2()
-                  .copyWith(color: ColorsTheme.of(context).chipTextBlack),
-            ),
-            const TextSpan(text: ' '),
-            TextSpan(
-              text:
-                  FlutterI18n.translate(context, 'background_service_solution'),
-              style: FontTheme.of(context)
-                  .subtitle1()
-                  .copyWith(color: ColorsTheme.of(context).chipTextBlack),
-            ),
-          ]),
+          children: texts),
       preferBelow: false,
       child: Icon(
         Icons.info_rounded,
