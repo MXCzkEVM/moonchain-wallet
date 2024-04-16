e24ac2ce787ba0407162461ffec0ce723018f7af
reasje <rezaaslejeddian@gmail.com>
2024-04-16 09:05:53 +0330

    refactor: chain config import

---
+++ lib/features/settings/subfeatures/chain_configuration/subfeatures/add_network/add_network.dart
@@ -0,0 +1,5 @@
+export 'add_network_state.dart';
+export 'add_network_presenter.dart';
+export 'add_network_page.dart';
+export 'widgets/widgets.dart';
+export 'utils/utils.dart';
---
+++ lib/features/settings/subfeatures/chain_configuration/widgets/widgets.dart
@@ -0,0 +1,5 @@
+export 'chain_logo_widget.dart';
+export 'chains_dialog.dart';
+export 'ipfs_gate_ways_dialog.dart';
+export 'network_details_page.dart';
+export 'network_item.dart';
