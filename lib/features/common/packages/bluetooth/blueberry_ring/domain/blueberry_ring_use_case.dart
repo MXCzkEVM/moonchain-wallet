e5558302feccf4e19fc0bd41070778d67b80a8d9
reasje <rezaaslejeddian@gmail.com>
2024-07-26 18:21:33 +0330

    fix: Blueberry name filter

--- lib/features/common/packages/bluetooth/blueberry_ring/domain/blueberry_ring_use_case.dart
+++ lib/features/common/packages/bluetooth/blueberry_ring/domain/blueberry_ring_use_case.dart
@@ -56,6 +56,8 @@ class BlueberryRingUseCase extends ReactiveUseCase {
     _bluetoothUseCase.startScanning(
       withServices: [bluetoothServiceUUID],
       // withNames: ['Mi Smart Band 4'],
+      withNames: ['2301']
+      
     );
 
     await Future.delayed(const Duration(seconds: 2), () async {
