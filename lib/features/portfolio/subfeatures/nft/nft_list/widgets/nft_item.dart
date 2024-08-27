1eba8f05d71be172bf64eef0c7edeae9059cf37d
reasje <rezaaslejeddian@gmail.com>
2024-08-27 22:45:49 +0330

    feat: Add domains support in NFT section

--- lib/features/portfolio/subfeatures/nft/nft_list/widgets/nft_item.dart
+++ lib/features/portfolio/subfeatures/nft/nft_list/widgets/nft_item.dart
@@ -1,6 +1,9 @@
+import 'package:datadashwallet/common/common.dart';
 import 'package:datadashwallet/features/portfolio/presentation/portfolio_page_presenter.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mxc_logic/mxc_logic.dart';
+import 'package:mxc_ui/mxc_ui.dart';
 
 class NFTItem extends HookConsumerWidget {
   const NFTItem({
@@ -9,38 +12,66 @@ class NFTItem extends HookConsumerWidget {
     this.size,
   });
 
-  final String imageUrl;
+  final String? imageUrl;
   final double? size;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(portfolioContainer.state);
+    final pattern = RegExp(r'^\w+\.\w+$');
+    final isDomainName =pattern.hasMatch(imageUrl?? '');
     return SizedBox(
       height: size ?? 105,
       width: size ?? 105,
       child: ClipRRect(
         borderRadius: BorderRadius.circular(10),
-        child: state.ipfsGateway != null
-            ? Image.network(
-                '${state.ipfsGateway}$imageUrl',
-                fit: BoxFit.fill,
-                errorBuilder: (context, error, stackTrace) => Text(
-                  error.toString(),
-                ),
-                loadingBuilder: (context, child, loadingProgress) {
-                  if (loadingProgress == null) return child;
-                  return Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: CircularProgressIndicator(
-                      value: loadingProgress.expectedTotalBytes != null
-                          ? loadingProgress.cumulativeBytesLoaded /
-                              loadingProgress.expectedTotalBytes!
-                          : null,
+        child: state.ipfsGateway != null && imageUrl != null
+            ? imageUrl!.startsWith('#')
+                ? FittedBox(
+                  fit: BoxFit.contain,
+                  child: Icon(
+                      Icons.hexagon,
+                      color: MXCColors.hexStringToColor(imageUrl!),
                     ),
-                  );
-                },
-              )
-            : Container(),
+                )
+                : isDomainName ? Container(
+                                        
+                                        decoration: BoxDecoration(
+                                          color: ColorsTheme.of(context).layerSheetBackground,
+                                          borderRadius: UIConfig.defaultBorderRadiusAll
+                                        ),
+                  child: FittedBox(
+                  fit: BoxFit.contain,
+                    child: Center(
+                      child: Text(
+                        imageUrl!,
+                          style: FontTheme.of(context).body1.textWhite(),
+                        ),
+                    ),
+                  ),
+                ) : Image.network(
+                    '${state.ipfsGateway}$imageUrl',
+                    fit: BoxFit.fill,
+                    errorBuilder: (context, error, stackTrace) => Text(
+                      error.toString(),
+                    ),
+                    loadingBuilder: (context, child, loadingProgress) {
+                      if (loadingProgress == null) return child;
+                      return Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: CircularProgressIndicator(
+                          value: loadingProgress.expectedTotalBytes != null
+                              ? loadingProgress.cumulativeBytesLoaded /
+                                  loadingProgress.expectedTotalBytes!
+                              : null,
+                        ),
+                      );
+                    },
+                  )
+            : Container(
+                padding: const EdgeInsets.all(Sizes.space2XSmall),
+                color: ColorsTheme.of(context).darkGray,
+                child: Image(image: ImagesTheme.of(context).appTextLogo)),
       ),
     );
   }
