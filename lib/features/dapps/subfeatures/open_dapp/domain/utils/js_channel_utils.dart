abe9c0c6ad771f0167d613694952fa66649228bd
reasje <rezaaslejeddian@gmail.com>
2024-10-31 15:26:11 +0330

    fix: Move js channel init to document start

--- lib/features/dapps/subfeatures/open_dapp/domain/utils/js_channel_utils.dart
+++ lib/features/dapps/subfeatures/open_dapp/domain/utils/js_channel_utils.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:mxc_logic/mxc_logic.dart';
 
 import '../../open_dapp.dart';
@@ -6,12 +7,13 @@ class JSChannelUtils {
   static void injectMXCWalletJSChannel(OpenDAppState state) async {
     // Making It easy for accessing axs wallet
     // use this way window.axs.callHandler
-    await state.webviewController!.evaluateJavascript(
-        source: JSChannelScripts.axsWalletObjectInjectScript(
-            JSChannelConfig.axsWalletJSObjectName));
+    // It was moved to web3 provider package to inject ad document start
+    // await state.webviewController!.evaluateJavascript(
+    //     source: JSChannelScripts.axsWalletObjectInjectScript(
+    //         JSChannelConfig.axsWalletJSObjectName));
 
-    await state.webviewController!.injectJavascriptFileFromAsset(
-        assetFilePath: 'assets/js/bluetooth/bluetooth.js');
+    // await state.webviewController!.injectJavascriptFileFromAsset(
+    //     assetFilePath: 'assets/js/bluetooth/bluetooth.js');
 
     // There is a gap for detecting the axs object in webview, It's intermittent after adding function structure to the scripts
     Future.delayed(
@@ -23,5 +25,9 @@ class JSChannelUtils {
         ));
       },
     );
+  }
+
+  static Future<String> loadJSBluetoothScript(BuildContext context) async {
+    return await DefaultAssetBundle.of(context).loadString('assets/js/bluetooth/bluetooth.js');
   }
 }
