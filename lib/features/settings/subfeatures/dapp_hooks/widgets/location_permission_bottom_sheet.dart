789831eea9a1960038b81af10b597fa4683f7627
reasje <rezaaslejeddian@gmail.com>
2024-12-14 14:25:49 +0330

    fix: Remove unnecessary padding from bottom sheets

--- lib/features/settings/subfeatures/dapp_hooks/widgets/location_permission_bottom_sheet.dart
+++ lib/features/settings/subfeatures/dapp_hooks/widgets/location_permission_bottom_sheet.dart
@@ -14,48 +14,43 @@ Future<bool?> showLocationPermissionBottomSheet({
     bottomSheetTitle: 'location_permission_required_title',
     hasCloseButton: false,
     widgets: [
-      Padding(
-        padding: const EdgeInsetsDirectional.symmetric(
-          horizontal: Sizes.spaceXLarge,
-        ),
-        child: Column(
-          children: [
-            Text(
-              translate(
-                'location_permission_required_text',
-              ),
-              style: FontTheme.of(context, listen: false)
-                  .body1
-                  .primary()
-                  .copyWith(),
-              softWrap: true,
-              textAlign: TextAlign.justify,
+      Column(
+        children: [
+          Text(
+            translate(
+              'location_permission_required_text',
             ),
-            const SizedBox(
-              height: Sizes.spaceNormal,
-            ),
-            MxcButton.primary(
-              key: const ValueKey('openLocationSettingsButton'),
-              title: translate('open_settings'),
-              onTap: () {
-                openLocationSettings();
-                Navigator.of(context).pop(true);
-              },
-              size: MXCWalletButtonSize.xl,
-            ),
-            const SizedBox(
-              height: Sizes.spaceNormal,
-            ),
-            MxcButton.secondary(
-              key: const ValueKey('cancelButton'),
-              title: translate('cancel'),
-              onTap: () {
-                Navigator.of(context).pop(true);
-              },
-              size: MXCWalletButtonSize.xl,
-            ),
-          ],
-        ),
+            style: FontTheme.of(context, listen: false)
+                .body1
+                .primary()
+                .copyWith(),
+            softWrap: true,
+            textAlign: TextAlign.justify,
+          ),
+          const SizedBox(
+            height: Sizes.spaceNormal,
+          ),
+          MxcButton.primary(
+            key: const ValueKey('openLocationSettingsButton'),
+            title: translate('open_settings'),
+            onTap: () {
+              openLocationSettings();
+              Navigator.of(context).pop(true);
+            },
+            size: MXCWalletButtonSize.xl,
+          ),
+          const SizedBox(
+            height: Sizes.spaceNormal,
+          ),
+          MxcButton.secondary(
+            key: const ValueKey('cancelButton'),
+            title: translate('cancel'),
+            onTap: () {
+              Navigator.of(context).pop(true);
+            },
+            size: MXCWalletButtonSize.xl,
+          ),
+        ],
       ),
     ],
   );
