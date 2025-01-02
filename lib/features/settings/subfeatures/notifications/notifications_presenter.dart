fcd2eff0281439867b5031d7b36b8866f2f189ef
reasje <rezaaslejeddian@gmail.com>
2025-01-02 15:26:15 +0330

    test: Remove notif page init actions

--- lib/features/settings/subfeatures/notifications/notifications_presenter.dart
+++ lib/features/settings/subfeatures/notifications/notifications_presenter.dart
@@ -59,21 +59,21 @@ class NotificationsPresenter extends CompletePresenter<NotificationsState>
     lowBalanceController.addListener(onLowBalanceChange);
     transactionFeeController.addListener(onTransactionFeeChange);
 
-    Future.delayed(
-      const Duration(
-        milliseconds: 1,
-      ),
-      () => showSnackBar(
-          context: context!,
-          content: translate(
-              'let_us_personalize_your_notifications_choose_which_ones_you_want_to_see')!),
-    );
+    // Future.delayed(
+    //   const Duration(
+    //     milliseconds: 1,
+    //   ),
+    //   () => showSnackBar(
+    //       context: context!,
+    //       content: translate(
+    //           'let_us_personalize_your_notifications_choose_which_ones_you_want_to_see')!),
+    // );
 
-    Future.delayed(
-        const Duration(
-          milliseconds: 1,
-        ),
-        () => notificationsHelper.checkNotificationsStatus());
+    // Future.delayed(
+    //     const Duration(
+    //       milliseconds: 1,
+    //     ),
+    //     () => notificationsHelper.checkNotificationsStatus());
   }
 
   void onLowBalanceChange() {
