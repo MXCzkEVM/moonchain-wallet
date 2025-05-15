1c7f5d317e7188333185cff0460a871832e3aa73
reasje <rezaaslejeddian@gmail.com>
2025-05-15 08:08:10 +0330

    Fix: Improve Portfolio screen Dock & snackbar fell oof screen

--- lib/features/portfolio/presentation/portfolio_page.dart
+++ lib/features/portfolio/presentation/portfolio_page.dart
@@ -24,162 +24,185 @@ class PortfolioPage extends HookConsumerWidget {
     final state = ref.watch(portfolioContainer.state);
 
     return MxcPage(
-        presenter: presenter,
-        resizeToAvoidBottomInset: true,
-        useGradientBackground: true,
-        layout: LayoutType.column,
-        useContentPadding: false,
-        childrenPadding: const EdgeInsets.only(top: 25, right: 24, left: 24),
-        appBar: AppNavBar(
-          action: IconButton(
-            key: const ValueKey('appsButton'),
-            icon: const Icon(MxcIcons.apps),
-            iconSize: 32,
-            onPressed: () =>
-                Navigator.of(context).replaceAll(route(const DAppsPage())),
-            color: ColorsTheme.of(context).iconPrimary,
-          ),
+      presenter: presenter,
+      resizeToAvoidBottomInset: true,
+      useGradientBackground: true,
+      layout: LayoutType.column,
+      useContentPadding: false,
+      childrenPadding: const EdgeInsets.only(top: 25, right: 12, left: 12),
+      appBar: AppNavBar(
+        action: IconButton(
+          key: const ValueKey('appsButton'),
+          icon: const Icon(MxcIcons.apps),
+          iconSize: 32,
+          onPressed: () =>
+              Navigator.of(context).replaceAll(route(const DAppsPage())),
+          color: ColorsTheme.of(context).iconPrimary,
         ),
-        floatingActionButton: Container(
-            width: MediaQuery.of(context).size.width,
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 30),
-            child: IntrinsicHeight(
-              child: Stack(
-                fit: StackFit.passthrough,
+      ),
+      children: [
+        Expanded(
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              Column(
                 children: [
-                  ClipRRect(
-                    borderRadius: const BorderRadius.all(Radius.circular(15)),
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          border:
-                              Border.all(color: ColorsTheme.of(context).white),
-                          borderRadius:
-                              const BorderRadius.all(Radius.circular(15)),
-                          color:
-                              ColorsTheme.of(context).white.withOpacity(0.02),
-                        ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 12),
+                      child: ListView(
+                        padding: const EdgeInsets.only(bottom: 160),
+                        children: [
+                          Text(FlutterI18n.translate(context, 'portfolio'),
+                              style: FontTheme.of(context).h4().copyWith(
+                                  fontSize: 34,
+                                  fontWeight: FontWeight.w400,
+                                  color: ColorsTheme.of(context).textPrimary)),
+                          const SizedBox(
+                            height: 6,
+                          ),
+                          const BalancePanel(true),
+                          const SizedBox(
+                            height: 32,
+                          ),
+                          Row(
+                            children: [
+                              MxcChipButton(
+                                key: const Key('tokensTabButton'),
+                                buttonState: state.switchTokensOrNFTs
+                                    ? ChipButtonStates.activeState
+                                    : ChipButtonStates.inactiveState,
+                                onTap: () =>
+                                    presenter.changeTokensOrNFTsTab(true),
+                                title: FlutterI18n.translate(context, 'tokens'),
+                                alignIconStart: true,
+                                mxcChipsEdgeType: MXCChipsEdgeType.hard,
+                                primaryColor: Colors.white,
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              MxcChipButton(
+                                key: const Key('nftsTabButton'),
+                                buttonState: !state.switchTokensOrNFTs
+                                    ? ChipButtonStates.activeState
+                                    : ChipButtonStates.inactiveState,
+                                onTap: () =>
+                                    presenter.changeTokensOrNFTsTab(false),
+                                title: FlutterI18n.translate(context, 'nfts'),
+                                alignIconStart: true,
+                                mxcChipsEdgeType: MXCChipsEdgeType.hard,
+                                primaryColor: Colors.white,
+                              )
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          if (state.switchTokensOrNFTs) ...[
+                            const TokensBalanceList(),
+                          ] else ...[
+                            NFTList(
+                              nfts: state.nftList,
+                            ),
+                          ]
+                        ],
                       ),
                     ),
                   ),
-                  Container(
-                    padding:
-                        const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        MXCRectangleButton.icon(
-                          key: const Key('sendButton'),
-                          icon: MxcIcons.send,
-                          iconFillColor: Colors.white,
-                          color: ColorsTheme.of(context).iconButtonInvertActive,
-                          onTap: () => Navigator.of(context).push(route(
-                              state.switchTokensOrNFTs
-                                  ? const ChooseCryptoPage()
-                                  : const ChooseNftPage())),
-                          titleStyle: FontTheme.of(context).subtitle1.primary(),
-                          iconSize: 24,
-                          filled: false,
-                          shadowRadius: 50,
-                          title: FlutterI18n.translate(context, 'send'),
+                ],
+              ),
+              // Overlay on top of the column layout
+              Positioned(
+                bottom: 40,
+                left: 0,
+                right: 0,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 20.0, sigmaY: 20.0),
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 4,
+                      ),
+                      width: MediaQuery.of(context).size.width,
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 8,
+                      ),
+                      decoration: BoxDecoration(
+                        border:
+                            Border.all(color: ColorsTheme.of(context).white),
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(15),
                         ),
-                        const SizedBox(
-                          width: 32,
-                        ),
-                        MXCRectangleButton.icon(
-                          key: const Key('receiveButton'),
-                          iconFillColor: Colors.white,
-                          color: ColorsTheme.of(context).iconButtonInvertActive,
-                          icon: MxcIcons.receive,
-                          onTap: () => presenter.showReceiveSheet(),
-                          titleStyle: FontTheme.of(context).subtitle1.primary(),
-                          iconSize: 24,
-                          filled: false,
-                          shadowRadius: 50,
-                          title: FlutterI18n.translate(context, 'receive'),
-                        ),
-                        const SizedBox(
-                          width: 32,
-                        ),
-                        MXCRectangleButton.icon(
-                          key: const Key('historyButton'),
-                          iconFillColor: Colors.white,
-                          color: ColorsTheme.of(context).iconButtonInvertActive,
-                          icon: MxcIcons.history,
-                          onTap: () => Navigator.of(context)
-                              .push(route(const TransactionHistoryPage())),
-                          titleStyle: FontTheme.of(context).subtitle1.primary(),
-                          iconSize: 24,
-                          filled: false,
-                          shadowRadius: 50,
-                          title: FlutterI18n.translate(context, 'history'),
-                        ),
-                      ],
+                        color: ColorsTheme.of(context).white.withOpacity(0.02),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          MXCRectangleButton.icon(
+                            key: const Key('sendButton'),
+                            icon: MxcIcons.send,
+                            iconFillColor: Colors.white,
+                            color:
+                                ColorsTheme.of(context).iconButtonInvertActive,
+                            onTap: () => Navigator.of(context).push(route(
+                                state.switchTokensOrNFTs
+                                    ? const ChooseCryptoPage()
+                                    : const ChooseNftPage())),
+                            titleStyle:
+                                FontTheme.of(context).subtitle1.primary(),
+                            iconSize: 24,
+                            filled: false,
+                            shadowRadius: 50,
+                            title: FlutterI18n.translate(context, 'send'),
+                          ),
+                          const SizedBox(
+                            width: 32,
+                          ),
+                          MXCRectangleButton.icon(
+                            key: const Key('receiveButton'),
+                            iconFillColor: Colors.white,
+                            color:
+                                ColorsTheme.of(context).iconButtonInvertActive,
+                            icon: MxcIcons.receive,
+                            onTap: () => presenter.showReceiveSheet(),
+                            titleStyle:
+                                FontTheme.of(context).subtitle1.primary(),
+                            iconSize: 24,
+                            filled: false,
+                            shadowRadius: 50,
+                            title: FlutterI18n.translate(context, 'receive'),
+                          ),
+                          const SizedBox(
+                            width: 32,
+                          ),
+                          MXCRectangleButton.icon(
+                            key: const Key('historyButton'),
+                            iconFillColor: Colors.white,
+                            color:
+                                ColorsTheme.of(context).iconButtonInvertActive,
+                            icon: MxcIcons.history,
+                            onTap: () => Navigator.of(context)
+                                .push(route(const TransactionHistoryPage())),
+                            titleStyle:
+                                FontTheme.of(context).subtitle1.primary(),
+                            iconSize: 24,
+                            filled: false,
+                            shadowRadius: 50,
+                            title: FlutterI18n.translate(context, 'history'),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ],
-              ),
-            )),
-        children: [
-          Expanded(
-              child: ListView(
-            padding: const EdgeInsets.only(bottom: 160),
-            children: [
-              Text(FlutterI18n.translate(context, 'portfolio'),
-                  style: FontTheme.of(context).h4().copyWith(
-                      fontSize: 34,
-                      fontWeight: FontWeight.w400,
-                      color: ColorsTheme.of(context).textPrimary)),
-              const SizedBox(
-                height: 6,
-              ),
-              const BalancePanel(true),
-              const SizedBox(
-                height: 32,
-              ),
-              Row(
-                children: [
-                  MxcChipButton(
-                    key: const Key('tokensTabButton'),
-                    buttonState: state.switchTokensOrNFTs
-                        ? ChipButtonStates.activeState
-                        : ChipButtonStates.inactiveState,
-                    onTap: () => presenter.changeTokensOrNFTsTab(true),
-                    title: FlutterI18n.translate(context, 'tokens'),
-                    alignIconStart: true,
-                    mxcChipsEdgeType: MXCChipsEdgeType.hard,
-                    primaryColor: Colors.white,
-                  ),
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  MxcChipButton(
-                    key: const Key('nftsTabButton'),
-                    buttonState: !state.switchTokensOrNFTs
-                        ? ChipButtonStates.activeState
-                        : ChipButtonStates.inactiveState,
-                    onTap: () => presenter.changeTokensOrNFTsTab(false),
-                    title: FlutterI18n.translate(context, 'nfts'),
-                    alignIconStart: true,
-                    mxcChipsEdgeType: MXCChipsEdgeType.hard,
-                    primaryColor: Colors.white,
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 12,
-              ),
-              if (state.switchTokensOrNFTs) ...[
-                const TokensBalanceList(),
-              ] else ...[
-                NFTList(
-                  nfts: state.nftList,
                 ),
-              ]
+              ),
             ],
-          ))
-        ]);
+          ),
+        ),
+      ],
+    );
   }
 }
